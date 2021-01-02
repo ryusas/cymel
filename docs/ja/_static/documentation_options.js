b311b46163159b6969a681dc22a51a3b2589554b
ryusas <20365380+ryusas@users.noreply.github.com>
2021-01-02 19:09:06 +0900

    Slightly improved when passing API 2.0 objects to O and Os.

--- docs/ja/_static/documentation_options.js
+++ docs/ja/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.2020123100',
+    VERSION: '0.1.2021010200',
     LANGUAGE: 'ja',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
