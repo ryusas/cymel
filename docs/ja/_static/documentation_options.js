04804ebfaf6cd846bdb8bf56c1d81a3b794d1a34
ryusas <20365380+ryusas@users.noreply.github.com>
2021-04-17 18:43:57 +0900

    0.4.2021041700

--- docs/ja/_static/documentation_options.js
+++ docs/ja/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.3.2021040700',
+    VERSION: '0.4.2021041700',
     LANGUAGE: 'ja',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
