b34c9c0721c85b5786b6abeae25183f0bac810ea
ryusas <20365380+ryusas@users.noreply.github.com>
2022-04-10 15:17:22 +0900

    0.10.2022041000

--- docs/ja/_static/documentation_options.js
+++ docs/ja/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.9.2022030500',
+    VERSION: '0.10.2022041000',
     LANGUAGE: 'ja',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
