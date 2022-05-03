39f95b92396ba3723aefb954ed21eaba9733296b
ryusas <20365380+ryusas@users.noreply.github.com>
2022-05-03 20:39:14 +0900

    Fixed setParent and addChild for joint.

--- python/cymel/core/cyobjects/dagnode.py
+++ python/cymel/core/cyobjects/dagnode.py
@@ -8,7 +8,7 @@ from __future__ import print_function
 
 from ...common import *
 from ..typeregistry import nodetypes, _FIX_SLOTS
-from .cyobject import BIT_DAGNODE
+from .cyobject import CyObject, BIT_DAGNODE
 from .dagnode_c import DagNodeMixin
 
 __all__ = ['DagNode']
@@ -39,7 +39,7 @@ class DagNode(DagNodeMixin, nodetypes.parentBasicNodeClass('dagNode')):
         """
         _setAttr(self.name() + '.v', False)
 
-    def setParent(self, parent=None, r=False, add=False, avoidJointShear=False):
+    def setParent(self, parent=None, r=False, add=False, avoidJointShear=False, unmaintainIS=False):
         u"""
         親ノードを変更する。
 
@@ -56,30 +56,80 @@ class DagNode(DagNodeMixin, nodetypes.parentBasicNodeClass('dagNode')):
             追加されるようにする。
             これは本来の Maya の挙動だが、このメソッドのデフォルトでは
             joint の shear を使用することで transform を追加しない。
+        :param `bool` unmaintainIS:
+            このノードが joint の場合に inverseScale 接続・切断の
+            保守をしない。
 
         .. warning::
             2019 全てと 2020.0 では、joint の shear は機能しないため、
             avoidJointShear=True を指定しないと姿勢を維持できない場合がある。
             この問題は 2020.1 以降はバグとして修正されている。
         """
-        if parent:
-            if self.isShape():
+        # shape なら普通にやるだけだが、parent 無しにはできない。
+        if self.isShape():
+            if parent:
                 _parent(self.name(), parent, s=True, r=True, add=add)
-            elif avoidJointShear or r or add or not self.isJoint():
+            else:
+                raise RuntimeError('A shape cannot be ungrouped.')
+
+        # joint でないか r=True が不要なら、普通にやるだけ。
+        elif add or not self.isJoint() or ((avoidJointShear or not r) and bool(r) is bool(unmaintainIS)):
+            # r=True,  unmaintainIS=True,  avoidJointShear=False : 普通に r=True でやる (pose も is も維持されない)
+            # r=True,  unmaintainIS=True,  avoidJointShear=True  : 普通に r=True でやる (pose も is も維持されない)
+            # r=False, unmaintainIS=False, avoidJointShear=True  : 普通に r=False でやる (transform追加で is も維持)
+            if parent:
                 _parent(self.name(), parent, r=r, add=add)
             else:
-                m = self.getM(ws=True)
-                _parent(self.name(), parent)  # r=True を指定すると is が接続されない。
-                self.setM(m, ws=True)
-        else:
-            if self.isShape():
-                raise RuntimeError('A shape cannot be ungrouped.')
-            elif avoidJointShear or r or add or not self.isJoint():
                 _parent(self.name(), w=True, r=r, add=add)
+
+        # joint で r=True が必要な場合は厄介（avoidJointShear=False の場合も r=True 扱い）。
+        else:
+            # r=False, unmaintainIS=True,  avoidJointShear=True  : transform追加で is 維持は無し -> 非サポート
+            # r=False, unmaintainIS=False, avoidJointShear=False : maintainM=1, maintainIS=1
+            # r=False, unmaintainIS=True,  avoidJointShear=False : maintainM=1, maintainIS=0
+            # r=True,  unmaintainIS=False, avoidJointShear=False : maintainM=0, maintainIS=1
+            # r=True,  unmaintainIS=False, avoidJointShear=True  : maintainM=0, maintainIS=1
+            self.__setJointParent(parent and CyObject(parent), not r, not unmaintainIS, avoidJointShear)
+
+    def __setJointParent(self, parent, maintainM, maintainIS, avoidSh):
+        # 元の姿勢を取得。
+        if maintainM:
+            if avoidSh:
+                raise ValueError(
+                    'Unsupported option value combinations: r=%r, unmaintainIS=%r, avoidJointShear=%r' %
+                    (not maintainM, not maintainIS, avoidSh))
+            m = self.getM(ws=True)
+
+        # 元の is の入力と標準的なものかどうかを判定。
+        if maintainIS:
+            srcIS = self.plug_('is').inputs()
+            if srcIS:
+                srcIS = srcIS[0]
+                if srcIS.shortName() == 's':
+                    curParent = self.parent()
+                    maintainIS = srcIS.node() == curParent and curParent.isJoint()
+                else:
+                    maintainIS = False
             else:
-                m = self.getM(ws=True)
-                _parent(self.name_(), w=True)  # r=True を指定すると is が接続されない。
-                self.setM(m, ws=True)
+                curParent = self.parent()
+                maintainIS = not curParent or not curParent.isJoint()
+
+        # parent 処理。r=True を指定すると is の切断や接続の処理がされない。
+        if parent:
+            _parent(self.name(), parent, r=True)
+        else:
+            _parent(self.name_(), w=True, r=True)
+
+        # 元の is が標準的なものだった場合、新しい親に対処する。
+        if maintainIS:
+            if parent and parent.isJoint():
+                self.plug_('is').connect(parent.s, force=True)
+            elif srcIS:
+                self.plug_('is').disconnect(srcIS, force=True)
+
+        # 姿勢を維持。
+        if maintainM:
+            self.setM(m, ws=True)
 
     def iterBreadthFirst(self, shapes=False, intermediates=False, underWorld=False):
         u"""
--- python/cymel/core/cyobjects/transform.py
+++ python/cymel/core/cyobjects/transform.py
@@ -27,7 +27,7 @@ class Transform(TransformMixin, nodetypes.parentBasicNodeClass('transform')):
 
     TYPE_BITS = BIT_DAGNODE | BIT_TRANSFORM  #: クラスでサポートしているノードの特徴を表す。
 
-    def addChild(self, child, r=False, add=False):
+    def addChild(self, child, r=False, add=False, avoidJointShear=False, unmaintainIS=False):
         u"""
         子ノードを追加する。複数指定可能。
 
@@ -38,23 +38,48 @@ class Transform(TransformMixin, nodetypes.parentBasicNodeClass('transform')):
             デフォルトではワールド空間で維持される。
         :param `bool` add:
             移動ではなくパスを追加する（インスタンス）。
+        :param `bool` avoidJointShear:
+            追加する子が joint で、ワールド姿勢の維持のために shear
+            が必要な場合に、その使用を避けるための transform が
+            追加されるようにする。
+            これは本来の Maya の挙動だが、このメソッドのデフォルトでは
+            joint の shear を使用することで transform を追加しない。
+        :param `bool` unmaintainIS:
+            このノードが joint の場合に inverseScale 接続・切断の
+            保守をしない。
         """
-        parent = self.name()
+        name = self.name()
+
+        # 子のシーケンスが指定された場合。
         if isinstance(child, Sequence) and not isinstance(child, BASESTR):
             others = [str(x) for x in child]
             shapes = _ls(others, type='shape')
+            # シェイプはまとめて処理してしまう。
             if shapes:
                 shapeSet =set(shapes)
-                others = [x for x in x not in shapeSet]
-                _parent(shapes, parent, s=True, r=True, add=add)
+                others = [x for x in others if x not in shapeSet]
+                _parent(shapes, name, s=True, r=True, add=add)
+            # その他は順番に処理する。
             if others:
-                _parent(others, parent, r=r, add=add)
+                if add or ((avoidJointShear or not r) and bool(r) is bool(unmaintainIS)) or not _ls(others, type='joint'):
+                    _parent(others, name, r=r, add=add)
+                else:
+                    for child in others:
+                        child = CyObject(child)
+                        if child.isJoint():
+                            child._DagNode__setJointParent(self, not r, not unmaintainIS, avoidJointShear)
+                        else:
+                            _parent(child.name_(), name, r=r, add=add)
+
+        # 単一の子が指定された場合。
         else:
             child = CyObject(child)
             if child.isShape():
-                _parent(child, parent, s=True, r=True, add=add)
+                _parent(child.name_(), name, s=True, r=True, add=add)
+            elif add or not child.isJoint() or ((avoidJointShear or not r) and bool(r) is bool(unmaintainIS)):
+                _parent(child.name_(), name, r=r, add=add)
             else:
-                _parent(child, parent, r=r, add=add)
+                child._DagNode__setJointParent(self, not r, not unmaintainIS, avoidJointShear)
 
 nodetypes.registerNodeClass(Transform, 'transform')
 
