7420e537ecab5b839bd0e81cac025f57485d27f4
ryusas <20365380+ryusas@users.noreply.github.com>
2022-10-10 19:22:07 +0900

    Update documentation

--- docs/ja/_static/documentation_options.js
+++ docs/ja/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.16.2022072400',
+    VERSION: '0.17.2022101000',
     LANGUAGE: 'ja',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
